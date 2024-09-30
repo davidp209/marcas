073e2fbfe692eaf2f31248f5594b597e9de99aa6
David <1918862@alu.murciaeduca.es>
2024-09-30 12:14:57 +0200

    20/09

---
+++ cliente/ejercicio2.js
@@ -0,0 +1,7 @@
+window.onload=function(){ 
+ let nombre = prompt("Introduce el nombre por pantalla");
+let edad = prompt("Introduce la edad")
+let esatadoCivil = prompt("Introduce el estado civil")
+
+console.log=alert(nombre + " tiene " + edad + " años y su estado civil es " + esatadoCivil
+)}
---
+++ cliente/ejercicio4.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+
+let precio, cantidad, resultado;
+
+precio = +(prompt("Mete el precio"));
+cantidad = +(prompt("Mete la cantidad"));
+
+resultado =  precio * cantidad;
+
+console.log(resultado)
+
+}
