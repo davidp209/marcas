0cd01f1d3ef34227363b2c2c85326a4ea184ec34
David <1918862@alu.murciaeduca.es>
2024-09-30 12:16:02 +0200

    ss

--- cliente/ejemplo1.js
+++
@@ -1,9 +0,0 @@
-window.onload=function(){ 
-let edad, nombre, esatadoCivil;
-edad =5;
-nombre = "juan";
-esatadoCivil = "viudo";
-
-console.log(nombre + " tiene " + edad + " años y su estado civil es " + esatadoCivil
-)
-}
